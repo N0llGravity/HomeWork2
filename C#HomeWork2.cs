b895c94cdcf0455015787627f68b2811e3b6ad5b
Artem <ar.toropov@mail.ru>
2022-12-09 01:06:48 +0300

    Finding second symbol in fractional number

--- C#HomeWork2.cs
+++ C#HomeWork2.cs
@@ -1,13 +1,24 @@
-﻿// Вывод второго знака из заданного числа
+﻿// Вывод второго знака из любого заданного числа
 
-double DigitCounter(double num)
+void DigitCounter(double num)
 {
     if(num < 0)
     {
-        num = num * - 1;
+        num = num * -1;                      //Math.Floor((n % 1) * 10)
     }
-    return Math.Floor(num);
-}
+    if (num < 10)
+    {
+        if(num == Math.Floor(num))
+        {
+            Console.WriteLine($"Number {num} has not a second symbol");
+        }
+        else
+        {
+            
+        }
+
+    }   
+}   
 
 double n = Convert.ToDouble(Console.ReadLine());
-Console.WriteLine(DigitCounter(n));
+DigitCounter(n);
