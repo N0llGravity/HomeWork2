d0b1b501a34e506134663525d7b5e5da50641b86
Artem <ar.toropov@mail.ru>
2022-12-09 03:58:49 +0300

    Ready

--- C#HomeWork2.cs
+++ C#HomeWork2.cs
@@ -10,38 +10,36 @@ double ReverseNegative(double num)         //проверяем число и п
     
 }
 
-void LessTen(double smallnum)              //Работа с дробными числами меньше 10
+void LessTen(double smallnum, double startnum)              //Работа с дробными числами меньше 10
     {
         if(smallnum == Math.Floor(smallnum))
         {
-            Console.WriteLine("Number has not a second digit");
+            Console.WriteLine($"Number {startnum} has not a second digit");
         }
         
         else
         {
             double secondDigit = Math.Floor((smallnum % 1) * 10);
-            Console.WriteLine($"second digit of this number is {secondDigit}");
+            Console.WriteLine($"Second digit in number {startnum} is {secondDigit}");
         }
 
     }
 
-void MoreTen(double bignum)                //Работа с числами больше 10
+void MoreTen(double bignum, double startnum)                //Работа числами больше 10
 {
     while(bignum > 100)
     {
         bignum = bignum / 10;
     }
-    bignum = bignum % 10;
-    Console.WriteLine($"second digit of this number is {bignum}"); 
+    double secondDigit = Math.Floor(bignum % 10);
+    Console.WriteLine($"Second digit in number {startnum} is {secondDigit}");
 }
 
 
+Console.WriteLine("Input any number ");     
 double n = Convert.ToDouble(Console.ReadLine());
-ReverseNegative(n);
-
-
-
-
-
-//Math.Floor((n % 1) * 10)
-// Console.WriteLine($"Number {num} has not a second digit");
+double reversed = ReverseNegative(n);
+if (reversed < 10)                                          //Сортировка числа по методам взависимости от величины
+LessTen(reversed, n);
+else
+MoreTen(reversed, n);
